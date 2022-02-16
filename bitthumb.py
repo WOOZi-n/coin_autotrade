4bb7f045934b9d586e04ced93d70594e4b630c27
LAPTOP-QNG3LN7H\jmjwj <jmjwj@naver.com>
2022-02-16 12:31:36 +0900

    [update] 상승장 알리미 추가

--- bitthumb.py
+++ bitthumb.py
@@ -1,8 +1,21 @@
 import pybithumb
-import time
-import datetime
 
-orderbook = pybithumb.get_orderbook("BTC")
-ms = int(orderbook["timestamp"])
-time = datetime.datetime.fromtimestamp(ms/1000)
-print(time)
+def bull_market(ticker):
+    data = pybithumb.get_ohlcv(ticker)
+    price = pybithumb.get_current_price(ticker)
+    ma5 = data['close'].rolling(5).mean()
+    last_ma5 = ma5[-2]
+
+    if last_ma5 < price:
+        return True
+    else:
+        return False
+
+
+tickers = pybithumb.get_tickers()
+for ticker in tickers:
+    is_bull = bull_market(ticker)
+    if is_bull:
+        print(ticker,"상승장")
+    else:
+        print(ticker,"하락장")
